1bff45fb52c598b6242f53b300a5d8ff2b3c9de4
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-15 17:15:51 -0700

    completed challenge 3 in code challenge 1

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -43,7 +43,12 @@ Use `forEach` to loop over the input array. The modified strings should each be 
 ------------------------------------------------------------------------------------------------ */
 
 const allUpperCase = (arr) => {
-  // Solution code here...
+  const newArray = [];
+
+  arr.forEach(string => {
+    newArray.push(string.toUpperCase());
+  });
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
