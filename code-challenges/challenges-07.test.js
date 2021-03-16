4b586a15b5b134a2b4742cf12393e14e1aa6ea3d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-16 14:54:24 -0700

    completed stretch #7

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -81,7 +81,7 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  return arr.map(value => value.charCodeAt());
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -253,7 +253,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
