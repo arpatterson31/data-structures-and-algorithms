337a246105eafc2d025a6f3315604dd8112328c7
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-19 22:02:00 -0700

    finished challenge 6

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -149,7 +149,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (str) => {
-  // Solution code here...
+  return str.split('').reduce((a, v) => v + a, '');
 };
 
 /* ------------------------------------------------------------------------------------------------
