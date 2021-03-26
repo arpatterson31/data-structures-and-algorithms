a948c03e57951e2f61b6ba7dcb5770fd4b12b9fa
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-25 20:27:09 -0700

    completed challenge 3

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -83,7 +83,7 @@ Write a function named checkValues that takes in an object and a value and retur
 ------------------------------------------------------------------------------------------------ */
 
 const checkValues = (obj, value) => {
-  // Solution code here...
+  return Object.values(obj).includes(value) ? true : false;
 };
 
 /* ------------------------------------------------------------------------------------------------
