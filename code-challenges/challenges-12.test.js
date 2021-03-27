e10dd96744e5f2c272313247f6d316a7eca02455
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-26 22:22:28 -0700

    completed challenge 4

--- code-challenges/challenges-12.test.js
+++ code-challenges/challenges-12.test.js
@@ -72,8 +72,15 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
-
+  let totalArray = [];
+  for(let i = 0; i < stores[0].length; i++){
+    let total = 0;
+    for(let j = 0; j < stores.length; j++){
+      total += stores[j][i];
+    }
+    totalArray.push(total);
+  }
+  return totalArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
