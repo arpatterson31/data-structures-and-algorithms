460ac6ccf04971a5e83f4397f6a03dfac8b86c9b
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-18 22:31:10 -0700

    completed #5

--- code-challenges/challenges-14.test.js
+++ code-challenges/challenges-14.test.js
@@ -138,7 +138,7 @@ https://secure.com returns true because the URL is secure
 https:/missingslash.org returns false because the URL is malformed
 ------------------------------------------------------------------------------------------------ */
 const isSecure = (url) => {
-  // Solution code here...
+  return /^(https:\/\/)/.test(url);
 };
 
 /* ------------------------------------------------------------------------------------------------
