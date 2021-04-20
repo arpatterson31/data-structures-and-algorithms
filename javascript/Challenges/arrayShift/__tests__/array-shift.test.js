b4ee8817fa3b0d320f1a2ed0d6c4ac614ddd6cf3
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-20 15:28:22 -0700

    fixed typo in test file

--- javascript/Challenges/arrayShift/__tests__/array-shift.test.js
+++ javascript/Challenges/arrayShift/__tests__/array-shift.test.js
@@ -4,11 +4,11 @@ const insertShiftArray = require('../array-shift.js');
 
 describe('ARRAY SHIFT TESTS:', () => {
 
-  it('handles test 1', () => {
+  it('Handles test 1', () => {
     expect(insertShiftArray([2,4,6,8], 5)).toEqual([2,4,5,6,8]);
   });
 
-  it('handles test 2', () => {
+  it('Handles test 2', () => {
     expect(insertShiftArray([4,8,15,23,42], 16)).toEqual([4,8,15,16,23,42]);
   });
 
