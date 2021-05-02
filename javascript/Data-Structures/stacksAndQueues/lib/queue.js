66827107509add45c1911b956597aa860ed17868
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-01 21:38:23 -0700

    set up enqueue, peek, isempty for queue, need to work on dequeue

--- javascript/Data-Structures/stacksAndQueues/lib/queue.js
+++ javascript/Data-Structures/stacksAndQueues/lib/queue.js
@@ -1,3 +1,34 @@
 'use strict';
 
 const Node = require('./node.js');
+
+class Queue {
+  constructor() {
+    this.length = 0;
+    this.next = null;
+  }
+
+  enqueue(value) {
+    let node = new Node(value);
+    this[this.length] = node;
+    this.length++;
+
+    return this;
+  }
+
+  dequeue() {
+    if(this.length === 0) return 'queue is empty';
+  }
+
+  peek(){
+    if(this.length === 0) return 'queue is empty';
+    return this[this.next];
+  }
+
+  isEmpty() {
+    if(this.length === 0) return true;
+    return false;
+  }
+}
+
+module.exports = Queue;
