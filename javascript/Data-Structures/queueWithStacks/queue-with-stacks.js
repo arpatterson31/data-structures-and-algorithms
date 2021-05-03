c7483e212acfd81c7eeacea3da25d68c6cb5ba27
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-03 15:27:01 -0700

    brought in my stack constructor file and started pseudoqueue constructor

---
+++ javascript/Data-Structures/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Stack = require('../stacksAndQueues/lib/stack.js');
+
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  enqueue(value) {
+
+  }
+
+}
+
+module.exports = PseudoQueue;
