f5232d70d30f6429d575cda0948f19e57f2e37ac
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-03 22:22:07 -0700

    wrote out enqueue and updated readme

--- javascript/Data-Structures/queueWithStacks/queue-with-stacks.js
+++ javascript/Data-Structures/queueWithStacks/queue-with-stacks.js
@@ -9,7 +9,15 @@ class PseudoQueue {
   }
 
   enqueue(value) {
+    while(this.stack1.length !== 0){
+      this.stack2.push(this.stack1.pop());
+    }
 
+    this.stack1.push(value);
+
+    while(this.stack2.length !==0){
+      this.stack1.push(this.stack2.pop());
+    }
   }
 
 }
