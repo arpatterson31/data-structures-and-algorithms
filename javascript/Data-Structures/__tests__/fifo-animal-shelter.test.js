9907abbc6b3c7480dbf9a0e500d76e1fc9a0e3ab
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-04 21:42:11 -0700

    code challenge finished

---
+++ javascript/Data-Structures/__tests__/fifo-animal-shelter.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { Animal, AnimalShelter } = require('../fifoAnimalShelter/fifo-animal-shelter.js');
+
+describe('ANIMAL SHELTER TESTS:', () => {
+
+  it('Can enqueue a cat', () => {
+    let shelter = new AnimalShelter();
+    let cat = new Animal('cat');
+    shelter.enqueue(cat);
+    expect(shelter.cat.length).toEqual(1);
+  });
+
+  it('Can enqueue a dog', () => {
+    let shelter = new AnimalShelter();
+    let dog = new Animal('dog');
+    shelter.enqueue(dog);
+    expect(shelter.dog.length).toEqual(1);
+  });
+
+  it('Can dequeue a cat', () => {
+    let shelter = new AnimalShelter();
+    let cat = new Animal('cat');
+    shelter.enqueue(cat);
+    shelter.enqueue(cat);
+    shelter.enqueue(cat);
+    shelter.dequeue(cat);
+    expect(shelter.cat.length).toEqual(2);
+  });
+
+  it('Can dequeue a dog', () => {
+    let shelter = new AnimalShelter();
+    let dog = new Animal('dog');
+    shelter.enqueue(dog);
+    shelter.enqueue(dog);
+    shelter.enqueue(dog);
+    shelter.dequeue(dog);
+    expect(shelter.dog.length).toEqual(2);
+  });
+
+  it('Should throw error msg when enqueing anything other than a cat or dog', () => {
+    let shelter = new AnimalShelter();
+    let bunny = new Animal('bunny');
+    expect(shelter.enqueue(bunny)).toBe('cats and dogs only');
+  });
+
+  it('Should return null when dequeing anything other than a cat or dog', () => {
+    let shelter = new AnimalShelter();
+    let bunny = new Animal('bunny');
+    expect(shelter.dequeue(bunny)).toBe(null);
+  });
+});
--- javascript/Data-Structures/fifoAnimalShelter/fifo-animal-shelter.js
+++ javascript/Data-Structures/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,10 +1,40 @@
 'use strict';
 
-class AnimalShelter {
-  constructor() {
-    this.dog = {};
-    this.cat = {};
+const Queue = require('../stacksAndQueues/lib/queue.js');
+
+class Animal {
+  constructor(type){
+    this.type = type;
   }
 }
 
-module.exports = AnimalShelter;
+class AnimalShelter {
+  constructor() {
+    this.dog = new Queue();
+    this.cat = new Queue();
+  }
+
+  enqueue(animal) {
+    if(animal.type === 'dog') {
+      this.dog.enqueue(animal);
+      return this.dog;
+    } else if(animal.type === 'cat') {
+      this.cat.enqueue(animal);
+      return this.cat;
+    } else {
+      return 'cats and dogs only';
+    }
+  }
+
+  dequeue(pref) {
+    if(pref.type === 'dog') {
+      return this.dog.dequeue(pref);
+    } else if(pref.type === 'cat') {
+      return this.cat.dequeue(pref);
+    } else {
+      return null;
+    }
+  }
+}
+
+module.exports = { Animal, AnimalShelter };
