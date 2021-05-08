19bfe1d58f7cb3e2aace2bffce4bf2536128b891
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-07 22:41:03 -0700

    completed the add in bst

--- javascript/Data-Structures/tree/lib/binary-search-tree.js
+++ javascript/Data-Structures/tree/lib/binary-search-tree.js
@@ -14,14 +14,28 @@ class BinarySearchTree {
     if (this.root === null) {
       this.root = node;
       return this;
-    } else {
-      // TODO
+    }
+    let current = this.root;
+    while(current){
+      if(value < current.value){
+        if(current.left === null){
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if(current.right === null){
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
     }
   }
 
   contains(value){
     if(this.root === null) return 'empty tree';
-    
+    // TODO
   }
 }
 
