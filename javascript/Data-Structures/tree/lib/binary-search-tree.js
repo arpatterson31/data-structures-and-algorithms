1832c7f14068bb446e0e084dd090405b024407da
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-05-07 22:04:26 -0700

    started classes for bt and bst

--- javascript/Data-Structures/tree/lib/binary-search-tree.js
+++ javascript/Data-Structures/tree/lib/binary-search-tree.js
@@ -1,3 +1,20 @@
 'use strict';
 
 const Node = require('./node.js');
+
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value){
+    // TODO
+  }
+
+  contains(value){
+    // TODO
+  }
+}
+
+module.exports = BinarySearchTree;
--- javascript/Data-Structures/tree/lib/binary-tree.js
+++ javascript/Data-Structures/tree/lib/binary-tree.js
@@ -1,3 +1,47 @@
 'use strict';
 
 const Node = require('./node.js');
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  preOrder(){
+    let results = [];
+
+    let _walk = node => {
+      results.push(node.value);
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return results;
+  }
+
+  inOrder(){
+    let results = [];
+
+    let _walk = node => {
+      if(node.left) _walk(node.left);
+      results.push(node.value);
+      if(node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return results;
+  }
+
+  postOrder(){
+    let results = [];
+
+    let _walk = node => {
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+      results.push(node.value);
+    };
+    _walk(this.root);
+    return results;
+  }
+}
+
+module.exports = BinaryTree;
